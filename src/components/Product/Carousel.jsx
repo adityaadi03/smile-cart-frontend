a68c23eb22bf44e45633eca605e398fdf5dae18c
aditya <aditya03pandey11@gmail.com>
2024-08-08 10:59:35 +0530

    minor changes

--- src/components/Product/Carousel.jsx
+++ src/components/Product/Carousel.jsx
@@ -38,7 +38,7 @@ const Carousel = () => {
     timerRef.current = setInterval(handleNext, 3000);
 
     return () => clearInterval(timerRef.current);
-  }, []);
+  });
 
   return (
     <div className="flex flex-col items-center">
